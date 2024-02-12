397215ff925071246968fb1c739131bc478b3321
qaladid <abdiaziz11410@gmail.com>
2024-02-12 23:54:48 +0300

    Initial commit

---
+++ loading_greent_data_2022.py
@@ -0,0 +1,60 @@
+import io
+import pandas as pd
+import requests  # Import the requests library
+import pyarrow.parquet as pq  # Import the pyarrow.parquet module
+
+# ... (your existing imports)
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    urls = [
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-01.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-02.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-03.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-04.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-05.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-06.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-07.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-08.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-09.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-10.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-11.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-12.parquet'
+    ]
+    
+    dfs = []  # List to store DataFrames from each URL
+    
+    for url in urls:
+        # Make an HTTP request to get the content of the Parquet file
+        response = requests.get(url)
+        
+        # Use pyarrow to read the Parquet file content into a Pandas DataFrame
+        parquet_file = pq.read_table(io.BytesIO(response.content))
+        df = parquet_file.to_pandas()
+        
+        dfs.append(df)
+    
+    # Concatenate all DataFrames into one
+    final_df = pd.concat(dfs, ignore_index=True)
+    
+    return final_df
+
+# ... (your existing test function)
+
+
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
